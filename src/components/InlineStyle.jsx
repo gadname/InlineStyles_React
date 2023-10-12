a0bfc4d5e7c7cf1ba21c8ad507436539fcf16319
GAKU <gakugaku1114@iCloud.com>
2023-10-12 15:10:57 +0000

    Update InlineStyle.jsx

--- src/components/InlineStyle.jsx
+++ src/components/InlineStyle.jsx
@@ -1,6 +1,6 @@
 export const InlineStyle = () => {
   const containerStyle = {
-    border: "solid 2px #392eff",
+    border: "solid 2px #392eff",っっs
     borderRadius: "20px",
     padding: "8px",
     margin: "8px",
